b17742829a236fe2230b300e81e2909bfe20918c
amal2004ik <163625354+amal2004ik@users.noreply.github.com>
2024-03-16 14:35:19 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,3 +1,4 @@
+User
 /*
  * Copyright (c) 2024 Your Name
  * SPDX-License-Identifier: Apache-2.0
@@ -16,14 +17,13 @@ module tt_um_HA (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-    // All output pins must be assigned. If not used, assign to 0.
-    // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-    assign ui_in[7:2] = 6'b000000; // Assigning the specific bits of ui_in to 0
-    assign uo_out[7:2] = 6'b000000; // Assigning the specific bits of uo_out to 0
-    assign uio_in = 8'b00000000; // Corrected assignment to assign all bits of uio_in to 0
-    assign uio_out = 8'b00000000; // Corrected assignment to assign all bits of uio_out to 0
-    assign uio_oe = 8'b00000000; // Corrected assignment to assign all bits of uio_oe to 0
-    assign uo_out[0] = ui_in[0] ^ ui_in[1]; // Assigning the sum of the first two bits of ui_in
-    assign uo_out[1] = ui_in[0] & ui_in[1]; // Assigning the carry of the first two bits of ui_in
-
-endmodule
+  // All output pins must be assigned. If not used, assign to 0.
+ // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+    assign ui_in[7:2]=6'b000000;
+    assign uo_out[7:2]=6'b000000;
+    assign uio_in = 0;
+  assign uio_out = 0;
+  assign uio_oe  = 0;
+    assign uo_out[0] = ui_in[0] ^ ui_in[1]; // assigning sum
+    assign uo_out[1] = ui_in[0] & ui_in[1]; // assigning carry
+endmodul
